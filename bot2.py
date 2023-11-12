3f923ea670f778300c229a8604c92ffa27d8dd2f
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2023-11-11 17:46:32 -0800

    init discord + simple web scraper

---
+++ bot2.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import discord
+from discord.ext import tasks
+from urllib.parse import urljoin
+
+
+TOKEN = 'YOUR_DISCORD_TOKEN'
+CHANNEL_ID = 1132481238960181300  
+URL = 'https://www.sixfootscience.com/brain-snips'
+testing = True
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@tasks.loop(minutes=1)
+async def check_blog():
+    response = requests.get(URL)
+
+    #beautiful soup literally does everything
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # titles + links
+    posts = soup.find_all('a', href=True)
+
+    for post in posts[:5] if testing else posts:
+        title = post.get_text(strip=True)
+        relative_link = f"{URL}{post['href']}"  #relative links suck
+        link = urljoin(URL, relative_link)
+        channel = client.get_channel(CHANNEL_ID)
+        
+        await channel.send(f"New article!!! {title}\nLink: {link}")
+
+@client.event
+async def on_ready():
+    print(f'bot started as {client.user.name} ({client.user.id})')
+    check_blog.start()
+
+client.run(TOKEN)
