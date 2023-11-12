8d90f2cda9ec638571a849ca755daf1bedd382e4
Christopher Zhang <82842634+Chrisandyu@users.noreply.github.com>
2023-11-11 20:03:07 -0800

    random title message + update discord token

--- bot2.py
+++ bot2.py
@@ -1,20 +1,33 @@
 import requests
+import os
+import random
 from bs4 import BeautifulSoup
 import discord
 from discord.ext import tasks
 from urllib.parse import urljoin
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("BOT_TOKEN")
+load_dotenv(find_dotenv())
+TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = 1132481238960181300  
 URL = 'https://www.sixfootscience.com/brain-snips'
-testing = True
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+phrases = [
+    "New post!!!",
+    "New blog update!",
+    "Read all about it!",
+    "Update:",
+    "Fresh post:",
+    "New article:",
+    "Our latest post:"
+    "Check it out:",
+    "New topic!!",
+    "New blog post:",
+    "New brain snip!",
+]
 
 @tasks.loop(minutes=1)
 async def check_blog():
@@ -22,16 +35,29 @@ async def check_blog():
 
     #beautiful soup literally does everything
     soup = BeautifulSoup(response.content, 'html.parser')
-    post = soup.find('a', href=True)
-    title = post.get_text(strip=True)
-    if(check_blog.title != title):
-        check_blog.title = title 
-        relative_link = f"{URL}{post['href']}" 
-        link = urljoin(URL, relative_link)
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send(f"New article!!! {title}\nLink: {link}")
 
+    title_element = soup.find('h1', class_='blog-title').find('a')
+
+    title = title_element.get_text(strip=True)
+
+    if check_blog.title != title:
+        check_blog.title = title 
+        print('new post: ' + title)
+
+        relative_link = title_element['href']
+        link = urljoin(URL, relative_link)
+
+        random_phrase = random.choice(phrases)
+        role = discord.utils.get(client.guilds[0].roles, name="Blog ping")
+        mention = role.mention
+
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send(f"{mention} **{random_phrase}** {title}\n{link}")
+
+check_blog.title = None
 @client.event
+
+
 async def on_ready():
     print(f'bot started as {client.user.name} ({client.user.id})')
     check_blog.start()
